c37e5f3efc188c02170bb68c11d805bb8479758b
Ian Maurmann <ikmxx@yahoo.com>
2025-01-17 23:24:42 -0700

    Add AE to AM

--- src/CountryList.php
+++ src/CountryList.php
@@ -36,15 +36,86 @@ class CountryList
 
     private function buildCountryList()
     {
+        // Properties
+        //     alpha-2          - ISO 3166-1 alpha-2 code
+        //     alpha-2__note    - Notes about the Alpha 2 code
+        //     ccTLD            - Top-Level Domain Name with leading dot
+        //     common_name      - Common Name in English
+        //     en_official_name - Official Name in English
+        //     normalized       - unique normalized human-readable name for lookup
+
+
         $countries = [
 
+            // AD
             // Andorra
             'andorra' => [
                 'normalized'       => 'andorra',
                 'alpha-2'          => 'AD',
                 'ccTLD'            => '.ad',
                 'common_name'      => 'Andorra',
-                'en_official_name' => 'Principality of Andorra'
+                'en_official_name' => 'Principality of Andorra',
+            ],
+
+            // AE
+            // United Arab Emirates
+            'emirates' => [
+                'normalized'       => 'emirates',
+                'alpha-2'          => 'AE',
+                'ccTLD'            => '.ae',
+                'common_name'      => 'United Arab Emirates',
+                'en_official_name' => 'United Arab Emirates',
+            ],
+
+            // AF
+            // Afghanistan
+            'afghanistan' => [
+                'normalized'       => 'afghanistan',
+                'alpha-2'          => 'AF',
+                'ccTLD'            => '.af',
+                'common_name'      => 'Afghanistan',
+                'en_official_name' => 'Islamic Emirate of Afghanistan',
+            ],
+
+            // AG
+            // Antigua and Barbuda
+            'antigua-and-barbuda' => [
+                'normalized'       => 'antigua-and-barbuda',
+                'alpha-2'          => 'AG',
+                'ccTLD'            => '.ag',
+                'common_name'      => 'Antigua and Barbuda',
+                'en_official_name' => 'Antigua and Barbuda',
+            ],
+
+            // AI
+            // Anguilla
+            'anguilla' => [
+                'normalized'       => 'anguilla',
+                'alpha-2'          => 'AI',
+                'alpha-2__note'    => 'AI previously represented French Afars and Issas, now Djibouti',
+                'ccTLD'            => '.ai',
+                'common_name'      => 'Anguilla',
+                'en_official_name' => 'Anguilla',
+            ],
+
+            // AL
+            // Albania
+            'albania' => [
+                'normalized'       => 'albania',
+                'alpha-2'          => 'AL',
+                'ccTLD'            => '.al',
+                'common_name'      => 'Albania',
+                'en_official_name' => 'Republic of Albania',
+            ],
+
+            // AM
+            // Albania
+            'armenia' => [
+                'normalized'       => 'armenia',
+                'alpha-2'          => 'AM',
+                'ccTLD'            => '.am',
+                'common_name'      => 'Armenia',
+                'en_official_name' => 'Armenia',
             ],
 
         ];
